5e6d8fe446ad5659cbee33053896246c5c88f7c3
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-09-10 12:53:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/team-project-7">
+        <BrowserRouter basename="/react_node_fs71_etopritika">
           <App />
         </BrowserRouter>
       </PersistGate>
